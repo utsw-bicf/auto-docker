ef16835a915bfed5d30dab07f15429f77722a3dc
jgesell <11574364+jgesell@users.noreply.github.com>
2021-03-09 12:09:15 -0600

    Standardized testing paths in test_update_relations.py.

--- tests/test_update_relations.py
+++ tests/test_update_relations.py
@@ -65,7 +65,7 @@ def test_get_children():
 
 @pytest.mark.test_get_parents
 def test_get_parents():
-    update_relations.DOCKERFILE_PATH = 'tests/Test_Dockerfile'
+    update_relations.DOCKERFILE_PATH = 'tests/1.0.0/Dockerfile'
     test_vars.append(update_relations.get_parents())
     assert test_vars[1] == ['ubuntu:18.04']
 
