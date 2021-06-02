faaf0c08e7f1022a5d2c65d6a2674393d3deeefd
Jon Gesell <11574364+jgesell@users.noreply.github.com>
2021-06-02 15:47:06 -0500

    Reverted the login.

--- scripts/functions.py
+++ scripts/functions.py
@@ -325,7 +325,8 @@ def pytest_cleanup(dockerfile_path):
 
 def docker_login():
     word_file = tempfile.NamedTemporaryFile()
-    word_file.write(os.environ.get('DOCKERHUB_PW'))
+    with open (word_file.name, 'w') as f:
+        f.write(os.environ.get('DOCKERHUB_PW'))
     print(word_file.name)
     if str(os.environ.get('DOCKERHUB_URL')).lower() == "none" or str(os.environ.get('DOCKERHUB_URL')).lower() == 'null' or os.environ.get('DOCKERHUB_URL') == None:
         print("DockerHub repository found, logging in.".format(
