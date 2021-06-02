0a1f286458d4142ac8e536d6516c35093ccfe50b
Jon Gesell <11574364+jgesell@users.noreply.github.com>
2021-06-02 14:02:54 -0500

    Added formatting to the call for the password secret.

--- scripts/functions.py
+++ scripts/functions.py
@@ -326,7 +326,7 @@ def pytest_cleanup(dockerfile_path):
 def docker_login():
     word_file = tempfile.NamedTemporaryFile()
     with open (word_file.name(), 'w'):
-        word_file.write(os.environ.get('DOCKERHUB_PW'))
+        word_file.write('{}'.format(os.environ.get('DOCKERHUB_PW')))
     if str(os.environ.get('DOCKERHUB_URL')).lower() == "none" or str(os.environ.get('DOCKERHUB_URL')).lower() == 'null' or os.environ.get('DOCKERHUB_URL') == None:
         print("DockerHub repository found, logging in.".format(
             os.environ.get('DOCKERHUB_URL')), file=sys.stderr)
