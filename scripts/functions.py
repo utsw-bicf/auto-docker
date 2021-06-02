631708f5dc2393052416babc5960c58d0e9a4e3c
Jon Gesell <11574364+jgesell@users.noreply.github.com>
2021-06-02 15:06:14 -0500

    Changed write function.

--- scripts/functions.py
+++ scripts/functions.py
@@ -325,7 +325,7 @@ def pytest_cleanup(dockerfile_path):
 
 def docker_login():
     word_file = tempfile.NamedTemporaryFile()
-    print(os.environ.get('DOCKERHUB_PW'), file=open(word_file.name(), 'w'))
+    word_file.write("{}".format(os.environ.get('DOCKERHUB_PW')))
     if str(os.environ.get('DOCKERHUB_URL')).lower() == "none" or str(os.environ.get('DOCKERHUB_URL')).lower() == 'null' or os.environ.get('DOCKERHUB_URL') == None:
         print("DockerHub repository found, logging in.".format(
             os.environ.get('DOCKERHUB_URL')), file=sys.stderr)
